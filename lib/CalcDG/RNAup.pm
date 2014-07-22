9c2cf4f710866affd59f088f6403688b39b3e24f
Rikki Guy <git@rikkiguy.me.uk>
2014-07-22 16:27:03 +0100

    Beginnings of RNAup wrapper

--- lib/CalcDG/RNAup.pm
+++ lib/CalcDG/RNAup.pm
@@ -1,15 +1,35 @@
 package CalcDG::RNAup;
 
+use IPC::Open3 qw( open3 );
+
 use Moo;
 use namespace::clean;
 
-has '_stdin' => ( is => 'rw' );
-has '_stdout' => ( is => 'rw' );
+has '_in' => ( is => 'rw' );
+has '_out' => ( is => 'rw' );
+has '_pid' => ( is => 'rw' );
 
 sub BUILD {
     my $self = shift;
     
+    my ($in, $out);
+    my $pid = open3($in, $out, $out, "RNAup -o -d 2 --noLP --include_both -c S");
     
+    $self->_in($in);
+    $self->_out($out);
+    $self->_pid($pid);
+}
+
+sub done {
+    my $self = shift;
+    
+    # RNAup quits upon sending the @ symbol
+    print $self->_in "@\n";
+    waitpid($self->_pid);
+    
+    $self->_din(undef);
+    $self->_dout(undef);
+    $self->_pid(undef);
 }
 
 1;
